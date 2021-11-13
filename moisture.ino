e8656cd5951194267413c3f049ac75609d658035
MedhaMadhusudhan <56746382+MedhaMadhusudhan@users.noreply.github.com>
2021-11-13 12:54:38 +0530

    Update moisture.ino

--- moisture.ino
+++ moisture.ino
@@ -1,10 +1,26 @@
+#include<Servo.h>
+Servo s;
+
 void setup(){
-  pinMode(A0, INPUT);
+  s.attach(10);
   Serial.begin(9600);
+  //pinMode(10,OUTPUT);
 }
 
 void loop(){
   int sensorVal = analogRead(A0);
-  Serial.println(sensorVal);
+ 
+  int pos;
+  if(sensorVal > 500){
+     Serial.println(sensorVal);
+    for(pos=0; pos<=180; pos++){
+      s.write(pos);
+      delay(15);
+    }
+    for(pos=180; pos>=0; pos--){
+      s.write(pos);
+      delay(15);
+    }
+  }
   delay(1000);
 }
